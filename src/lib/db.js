99dbf3857d696f9d368078ae6bbdc6851cbdc209
tuxdinosaur <ivan.tux@outlook.com>
2020-05-21 00:43:48 -0500

    :zap: [UPDATE] Se actualizó el password de DB

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose')
 
-const DB_PASSWORD = 'NuvoIvan3472'
+const DB_PASSWORD = '7TbtOmkabUQqMH8B'
 const DB_NAME = 'toroto_db'
 
 const url = `mongodb+srv://nuvoivan:${DB_PASSWORD}@toroto-m0sbe.azure.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
